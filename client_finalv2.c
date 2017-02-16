80b4189bddd0bc621b455e8a70cc67a201509760
kchin28 <kgchin@ualberta.ca>
2017-02-15 18:09:03 -0700

    data collected ; working with test.sh

---
+++ client_finalv2.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>
+
+int* seed;
+int aSize=100;
+int usr_port;
+
+void *connectServer(void *args){
+	struct sockaddr_in sock_var;
+	int clientFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
+	long rank = (long)args;
+	char buff[100];
+	
+	sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
+	sock_var.sin_port=usr_port;
+	sock_var.sin_family=AF_INET;
+
+	
+	if(connect(clientFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
+	{
+		int pos = rand_r(&seed[rank]) % aSize ;
+		int oper = rand_r(&seed[rank]) % 100 ;
+
+		if(oper<95){
+			if(snprintf(buff,100,"%d %c",pos,'r')<0){
+				printf("ERROR: client could not write to buffer. \n");
+				return(NULL);			
+			}		
+		}
+		else if(oper>=95){
+			if(snprintf(buff,100,"%d %c",pos,'w')<0){
+				printf("ERROR: client could not write to buffer. \n");
+				return(NULL);			
+			}	
+		}
+		else{
+			if(snprintf(buff,100,"%d %c",pos,'e')<0){
+				printf("ERROR: client could not write to buffer. \n");
+				return(NULL);			
+			}	
+		}
+
+		write(clientFileDescriptor,buff,100);
+
+		//handle server request result
+		char result[100];
+		read(clientFileDescriptor,result,100);
+		//printf("%s\n",result);
+		
+	}
+	else{
+		printf("socket connection failed\n");
+	}
+	close(clientFileDescriptor);
+	return(NULL);
+}
+
+int main(int argc, char * argv []) 
+//first argument: server second argument: size of array (n) 10, 100, 1000, and 10,000
+{
+	int n = atoi(argv[2]);
+	usr_port = atoi(argv[1]);
+
+	if( (n==10)||(n==100)||(n==1000)||(n==10000) ){              //default:100
+		aSize=n;	
+	}
+	
+
+	long thread;
+	pthread_t thread_handles[1000];
+
+	seed = malloc(1000*sizeof(int)); 
+
+	for (thread = 0; thread < 1000; thread++) {
+		seed[thread] = thread;
+		pthread_create(&thread_handles[thread], NULL, connectServer, (void*) thread);  			
+	}
+
+	for (thread = 0; thread < 1000; thread++) {
+		pthread_join(thread_handles[thread], NULL);  			
+	}
+	free(seed);
+	return 0;
+}
+
---
+++ server_mutex_entire_finalv2.c
@@ -0,0 +1,145 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>
+#include "timer.h"
+
+//gcc -std=gnu99  server_mutex_entire_finalv2.c -o smefv2 -lpthread -Wall
+
+char** theArray;
+pthread_mutex_t mutex;
+int aSize = 100;
+int usr_port;
+
+void* ServerEcho(void *args);
+
+int main(int argc, char * argv []){
+
+	int n = atoi(argv[2]);
+	usr_port = atoi(argv[1]);
+
+	if( (n==10)||(n==100)||(n==1000)||(n==10000) ){ //default:100
+		aSize=n;	
+	}
+
+    struct sockaddr_in sock_var;
+    int serverFileDescriptor=socket(AF_INET,SOCK_STREAM,0);
+    int clientFileDescriptor;
+
+    sock_var.sin_addr.s_addr=inet_addr("127.0.0.1");
+    sock_var.sin_port=usr_port;
+    sock_var.sin_family=AF_INET;
+
+    pthread_t* thread_handles;
+    double start,end,time;
+
+    if(bind(serverFileDescriptor,(struct sockaddr*)&sock_var,sizeof(sock_var))>=0)
+    {
+        printf("socket has been created \n");
+        listen(serverFileDescriptor,2000); 
+
+        while(1){
+
+            pthread_mutex_init(&mutex,NULL);
+            thread_handles = malloc(1000*sizeof(pthread_t));
+
+            theArray=malloc(aSize*sizeof(char*));  //hardcode array size for now
+            for (int i=0; i<aSize;i++){
+                theArray[i]=malloc(100*sizeof(char)); //use 100 for msg
+                sprintf(theArray[i], "%s%d%s", "String ", i, ": the initial value" );
+            }   
+
+            GET_TIME(start);
+
+            for(int i=0;i<1000;i++){
+                clientFileDescriptor=accept(serverFileDescriptor,NULL,NULL);
+             //   printf("Connected to client %d \n",clientFileDescriptor);
+                pthread_create(&thread_handles[i],NULL,ServerEcho,(void*)clientFileDescriptor);
+            }   
+
+            for(int i = 0;i<1000;i++){
+                pthread_join(thread_handles[i],NULL);
+            }
+  		
+            GET_TIME(end);
+            time = end-start;
+            printf(" %f \n",time);
+
+ //   		printf("All threads have joined. Checking Array \n");
+ //   		for(int i=0;i<aSize;i++){
+ //   			printf("%s\n",theArray[i]);		
+ //   		}
+
+            for (int i=0; i<aSize;i++){
+                free(theArray[i]);
+            }   
+           	free(theArray);
+	        free(thread_handles);
+            pthread_mutex_destroy(&mutex);
+
+        }//end of infinite while loop
+
+    }
+    else{
+        printf("socket creation failed \n");
+        return 1;
+    }
+
+    return 0;
+
+} //end of main
+
+void* ServerEcho(void* args){
+    long clientFileDescriptor = (long) args;
+
+    char buff[100];
+    read(clientFileDescriptor,buff,100);
+
+    int pos;
+    char operation;
+    int CSerror;
+   
+    sscanf(buff, "%d %c", &pos,&operation);
+ //   printf("   Server thread received position %d and operation %c from %ld \n",pos,operation,clientFileDescriptor);
+
+    if(operation=='r'){ //read operation
+        char msg[100];
+
+        pthread_mutex_lock(&mutex);
+            CSerror=snprintf(msg,100, "%s", theArray[pos] );
+        pthread_mutex_unlock(&mutex);
+
+        if(CSerror<0){
+            printf("ERROR: could not read from position: %d \n", pos);
+            sprintf(msg, "%s %d","Error writing to pos: ", pos );     
+        }
+
+        write(clientFileDescriptor,msg,100);
+        close(clientFileDescriptor);    
+    }
+    else if(operation=='w'){ //write operation
+        char msg[100];
+        snprintf(msg,100, "%s%d%s","String ", pos, " has been modified by a write request \n" );
+
+        pthread_mutex_lock(&mutex);
+            CSerror=snprintf(theArray[pos],100,"%s",msg);
+        pthread_mutex_unlock(&mutex);
+
+        if(CSerror<0){
+            printf("ERROR: could not write to array \n");
+            sprintf(msg, "%s %d","Error writing to pos: ", pos );     
+        }
+
+        write(clientFileDescriptor,msg,100);
+        close(clientFileDescriptor);           
+    }
+    else{
+        printf("ERROR: could not communicate with client %ld \n",clientFileDescriptor);  
+    }
+
+    return NULL;
+}
